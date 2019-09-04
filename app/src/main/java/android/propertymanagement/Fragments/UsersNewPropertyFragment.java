3ac82213a7692a6fe1bf7bb15c62088eafd8cbff
Naga Venkatesh <venkateshnaga93@gmail.com>
2019-09-04 12:13:02 +0530

    users UI Screens

--- app/src/main/java/android/propertymanagement/Fragments/UsersNewPropertyFragment.java
+++ app/src/main/java/android/propertymanagement/Fragments/UsersNewPropertyFragment.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.propertymanagement.R;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class UsersNewPropertyFragment extends Fragment {
+public class UsersNewPropertyFragment extends Fragment implements  View.OnClickListener{
 
 
     private Context mContext;
     View rootView;
+    private AlertDialog alertDialog, alert;
+
 
 
     @Override
@@ -32,5 +35,74 @@ public class UsersNewPropertyFragment extends Fragment {
     private void initViews() {
 
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.about_tv:
+
+                /*
+                 *  Custom dialog for aboutus
+                 */
+                aboutUsDialog();
+
+                break;
+
+            case R.id.terms_of_use_tv:
+
+                /*
+                 * Custom dialog for terms of use
+                 */
+                termsOfUseDialog();
+
+                break;
+
+            case R.id.privacy_policy_tv:
+
+                /*
+                 * Custom dialog for privacy policy
+                 */
+                privacyPolicyDialog();
+
+                break;
+
+        }
+
+    }
+
+    private void aboutUsDialog() {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_about_us, null);
+        dialogBuilder.setView(dialogView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void termsOfUseDialog() {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_terms_of_use, null);
+        dialogBuilder.setView(dialogView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void privacyPolicyDialog() {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_privacy_policy, null);
+        dialogBuilder.setView(dialogView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+    }
+
+
+
 
 }
